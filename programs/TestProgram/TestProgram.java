1ea99439b72a155b14fd5b80348b36e7ed1814ad
nayan-agrawal <nayanagrawal939@gmail.com>
2024-01-22 22:05:32 +0530

    Arrays in Java fundamentals

--- programs/TestProgram/TestProgram.java
+++ programs/TestProgram/TestProgram.java
@@ -5,11 +5,8 @@ package programs.TestProgram;
  */
 public class TestProgram {
     public static void main(String[] args) {
-        int i = 2;
-        while (i >= 0) {
-            main(args);
-            System.out.println("GFG");
-            --i;
-        }
+        int arr[] = {1,2};
+        int arr1[] = {1,2};
+        System.out.println(arr[0] + " " + arr[1]);
     }
 }
